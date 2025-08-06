91e0489606a67d90fb0a28310b9d9b105e0bebf3
Hasan <hasan@Hasans-MacBook-Air.local>
2025-08-06 10:03:59 +0530

    commit

---
+++ redfingers/src/data/data.js
@@ -0,0 +1,23 @@
+// src/data/data.js
+
+export const modeData = {
+  Chants: [
+    "You’ll Never Walk Alone",
+    "Allez Allez Allez",
+    "We Are Liverpool",
+    "Fields of Anfield Road"
+  ],
+  Quotes: [
+    "My idea was to play beautiful football — and win.",
+    "We have to change from doubters to believers.",
+    "Success is no accident.",
+    "Playing at Anfield is something else."
+  ],
+  Players: [
+    "Mohamed Salah",
+    "Steven Gerrard",
+    "Virgil van Dijk",
+    "Luis Díaz",
+    "Trent Alexander-Arnold"
+  ]
+};
